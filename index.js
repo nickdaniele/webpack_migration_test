7693193999ac2612eac37d6f2e8b21342ff3fa1c
Nick Daniele <ngdaniele@gmail.com>
2018-05-12 16:05:23 -0700

    Root html and js files

---
+++ index.js
@@ -0,0 +1,14 @@
+/* eslint-disable */
+
+requirejs.config({
+	paths: {
+		text: 'node_modules/requirejs-text/text',
+		jquery: 'node_modules/jquery/dist/jquery',
+		underscore: 'node_modules/underscore/underscore',
+		backbone: 'node_modules/backbone/backbone',
+		'backbone.radio': 'node_modules/backbone.radio/build/backbone.radio',
+		'backbone.marionette': 'node_modules/backbone.marionette/lib/backbone.marionette'
+	}
+});
+
+requirejs(['js/app']);
